319814cb56f5e57a6c0a33b7654160760da43a6e
Julio Meza <juliomeza@gmail.com>
2022-03-09 18:32:22 -0500

    Moving files

--- charts.js
+++ charts.js
@@ -3,7 +3,7 @@ function init() {
   var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
-  d3.json("samples.json").then((data) => {
+  d3.json("data/samples.json").then((data) => {
     console.log('Init Function');
     var sampleNames = data.names;
 
@@ -33,7 +33,7 @@ function optionChanged(newSample) {
 
 // Demographics Panel 
 function buildMetadata(sample) {
-  d3.json("samples.json").then((data) => {
+  d3.json("data/samples.json").then((data) => {
     console.log('buildMetadata Function');
     var metadata = data.metadata;
     // Filter the data for the object with the desired sample number
@@ -58,7 +58,7 @@ function buildMetadata(sample) {
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("samples.json").then((data) => {
+  d3.json("data/samples.json").then((data) => {
     console.log('buildCharts Function');
     // 3. Create a variable that holds the samples array.
     var sampleSamples = data.samples;
